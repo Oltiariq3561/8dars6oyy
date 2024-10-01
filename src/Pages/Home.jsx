3b41b7687479d5a539e8a19e66d656862e0392b0
Oltiariq3561 <oltiariq356@gmail.com>
2024-09-30 22:13:17 -0700

    salom

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -43,20 +43,20 @@ function Home() {
         <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
             <form onSubmit={handleAddProduct} className="bg-white p-6 rounded shadow-md space-y-4 w-full max-w-md">
                 <h2 className="text-xl font-bold">Add Product</h2>
-                <input ref={nameRef} type="text" placeholder="Product Name" required className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"/>
-                <input ref={descriptionRef} type="text" placeholder="Description" required className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500" />
-                <input ref={priceRef} type="number" placeholder="Price" required className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500" />
-                <input ref={categoryRef} type="number" placeholder="Category ID" required className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500" />
+                <input ref={nameRef} type="text" placeholder="Product ..." required className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"/>
+                <input ref={descriptionRef} type="text" placeholder="Description..." required className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500" />
+                <input ref={priceRef} type="number" placeholder="Price..." required className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500" />
+                <input ref={categoryRef} type="number" placeholder="ID..." required className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500" />
                 <button type="submit" className="bg-gray-700 text-white py-3 rounded-md hover:bg-gray-600 transition-all duration-200 w-full" > Add </button>
             </form>
 
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-8 w-full max-w-6xl">
-                {products.map((product) => (
-                    <div key={product.id} className="border border-gray-300 p-4 rounded-md bg-white shadow-md">
-                        <h3 className="text-lg font-semibold">{product.name}</h3>
-                        <p>{product.description}</p>
-                        <p className="font-bold">${product.price}</p>
-                        <p>ID: {product.category_id}</p>
+                {products.map((value) => (
+                    <div key={value.id} className="border border-gray-300 p-4 rounded-md bg-white shadow-md">
+                        <h3 className="text-lg font-semibold">{value.name}</h3>
+                        <p>{value.description}</p>
+                        <p className="font-bold">${value.price}</p>
+                        <p>ID: {value.category_id}</p>
                     </div>
                 ))}
             </div>
